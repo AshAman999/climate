b0deeed951d834a7cd418271af4a6b023495896a
Aman Raj <amanpie9@gmail.com>
2021-12-21 19:08:40 +0530

    api calls implemented

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'dart:convert';
+import 'package:climate/searchscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+String apikey = '';
+String temp = '0';
+String humidity = '0';
+String maxTemp = '0';
+String minTemp = '0';
+String cityName = 'Delhi';
+
+Future<void> callApi() async {
+  print('Data api call is being done');
+  var response = await http.get(
+    Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apikey&units=metric'),
+  );
+  print(response.body);
+  var weatherData = jsonDecode(response.body);
+  print(weatherData['main']['temp']);
+  temp = weatherData['main']['temp'].toString();
+  humidity = weatherData['main']['humidity'].toString();
+  maxTemp = weatherData['main']['temp_max'].toString();
+  minTemp = weatherData['main']['temp_min'].toString();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    setState(() {
+      callApi();
+    });
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        leading: Icon(Icons.favorite),
+        title: Text('Weather App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              var city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SearchScreen()),
+              );
+              print(city);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$temp°  ',
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              ' C',
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            Text(
+              cityName.toUpperCase(),
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+              ),
+            ),
+            Container(
+              height: 70,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sunny',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Humidity = $humidity %',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Max = $maxTemp°C',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Min = $minTemp °C',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
